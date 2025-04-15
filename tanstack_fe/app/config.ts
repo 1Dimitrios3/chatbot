c70c950db914d80124e170c3564583ba8698adb4
Dimitris Mavrokefalos <dimitrismavrokefalos@mac.station>
2025-04-15 11:43:52 +0300

    update chat model list with latest

--- tanstack_fe/app/config.ts
+++ tanstack_fe/app/config.ts
@@ -1,6 +1,7 @@
 const modelOptions = [
     { label: "GPT-4o-mini", value: "gpt-4o-mini" },
-    { label: "GPT-4", value: "gpt-4" },
+    { label: "GPT-4.1", value: "gpt-4.1" },
+    { label: "GPT-4.1-mini", value: "gpt-4.1-mini" },
     { label: "GPT-4o", value: "gpt-4o" },
     { label: "GPT-3.5 Turbo", value: "gpt-3.5-turbo" }
   ];
